b3101e0f0f72ecfedf858fe64b1360ea77f119e0
Mohsin <mdmr207570@gmail.com>
2026-01-06 10:39:06 +0600

    Complate Assignment

--- labtest.cpp
+++ labtest.cpp
@@ -3,13 +3,28 @@ using namespace std;
 //write a user defined function to find first N prime numbers;
 //the function should be named like : youname_yourid;
 
-/*
-example:
 
-void nishat_90(int n){
-//your code here;
+void mohsin_231400003(int n) {
+    int count = 0;
+    int num = 2;
+
+    while (count < n) {
+        bool isPrime = true;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << num << " ";
+            count++;
+        }
+        num++;
+    }
 }
-*/
 
 int main(){
 
@@ -17,4 +32,5 @@ int main(){
     int n;
     cout<<"Enter the number n"<<endl;
     cin>>n;
+    mohsin_231400003(n);
 }
